d89a6c2e5f9d0ff732158c4bc28ab858b0cf30cf
agusbauer <agustinbauer1@gmail.com>
2016-03-19 17:25:43 -0300

    schedulesList update

--- js/controllers/schedulesListController.js
+++ js/controllers/schedulesListController.js
@@ -7,12 +7,15 @@
 	});
 
 	 var schedules = [
-	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: "Disponible"},
-	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: "Disponible"},
-	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: "Disponible"},
-	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: "Disponible"},
-	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: "Disponible"},
-	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: "Disponible"}
+	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: true},
+	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: true},
+	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: true},
+	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: false},
+	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: true},
+	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: false},
+	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: true},
+	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: false},
+	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: true}
   	];
 
 })();
