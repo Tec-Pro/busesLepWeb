680fe035e7fdc4f0933cb4c9e4082a2e1327feee
agusbauer <agustinbauer1@gmail.com>
2016-03-19 17:01:12 -0300

    schedulesListContrller

---
+++ js/controllers/schedulesListController.js
@@ -0,0 +1,18 @@
+(function(){
+
+	var schedulesList = angular.module('schedulesList', []);
+
+	schedulesList.controller('ScheduleController', function(){
+  		this.products = schedules;
+	});
+
+	 var schedules = [
+	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: "Disponible"},
+	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: "Disponible"},
+	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: "Disponible"},
+	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: "Disponible"},
+	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: "Disponible"},
+	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: "Disponible"}
+  	];
+
+})();
