e0fc6a33101dec121186f77a6ab83fb6bd3365c3
agusbauer <agustinbauer1@gmail.com>
2016-03-29 17:18:50 -0300

    view conections with lastSearches

--- js/controllers/lastSearchesController.js
+++ js/controllers/lastSearchesController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('LastSearchesController', function ($scope){
+angular.module('app').controller('LastSearchesController', function ($scope, $location){
   	$scope.searches = [
 	    { goingDate: '10/10/2016', backDate: '07/03/2202', goingCity: 'Almafuerte', backCity: 'Rio Cuarto', status: true},
 	    { goingDate: '20/20/2010', backDate: '23/10/3303', goingCity: 'Almafuerte', backCity: 'Rio Cuarto', status: true},
@@ -10,4 +10,10 @@ angular.module('app').controller('LastSearchesController', function ($scope){
 	    { goingDate: '20/20/2010', backDate: '23/10/3303', goingCity: 'Almafuerte', backCity: '', status: false},
 	    { goingDate: '30/30/2020', backDate: '12/04/2123', goingCity: 'Cordoba', backCity: 'Rio Cuarto', status: true}
   	];
+
+  	$scope.goSchedules = function() {
+	    $location.path('/schedules');	 
+	    //seat.img = '../img/occupied_seat.png'
+	};
+
 });
