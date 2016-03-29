26680edb3ffbcf04d4d9beb4aab70aaa567e727c
agusbauer <agustinbauer1@gmail.com>
2016-03-29 17:01:27 -0300

    schedules and summary conections

--- js/controllers/schedulesListController.js
+++ js/controllers/schedulesListController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('ScheduleController', function ($scope){
+angular.module('app').controller('ScheduleController', function ($scope, $location){
   	$scope.schedules = [
 	    { departureDate: '10/10/2016', arrivalDate: '07/03/2202', departureTime: '18:00', arrivalTime: '20:00', status: true},
 	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: true},
@@ -10,4 +10,9 @@ angular.module('app').controller('ScheduleController', function ($scope){
 	    { departureDate: '20/20/2010', arrivalDate: '23/10/3303', departureTime: '07:00', arrivalTime: '12:00', status: false},
 	    { departureDate: '30/30/2020', arrivalDate: '12/04/2123', departureTime: '13:00', arrivalTime: '21:00', status: true}
   	];
+
+  	$scope.goSummary = function() {
+	    $location.path('/summary');	 
+	    //seat.img = '../img/occupied_seat.png'
+	};
 });
