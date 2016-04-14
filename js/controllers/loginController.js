d3ad47c4ff666be4766cae2583ab3c208826bb6e
jjaimez <jacinto.riocuarto@gmail.com>
2016-04-14 13:03:59 -0300

    ws jazu

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('LoginCtrl', function ($scope , $location, localStorageService) {
+.controller('LoginCtrl', function ($scope , $location, localStorageService,tripService) {
     
    if(localStorageService.get("user-lep")){
         $scope.user = localStorageService.get("user-lep");
@@ -22,10 +22,15 @@ angular.module('app')
     };
 
     $scope.login = function ( ) {
-       $scope.user.email = "PPPPPP@gmail.com";
-        localStorageService.set("user-lep", $scope.user);
-        localStorageService.set("rld", "yes");
-        location.reload();
+        tripService.login($scope.user.dni,$scope.user.pass).then(function(origins){
+          $scope.user.name = origins[0].Nombre;
+          $scope.user.lastname = origins[0].Apellido;
+          $scope.user.email = origins[0].Email;
+          localStorageService.set("user-lep", $scope.user);
+          localStorageService.set("rld", "yes");
+          location.reload();
+        });
+        
     };
 
     $scope.logout = function ( ) {
@@ -35,8 +40,15 @@ angular.module('app')
 
     $scope.signin = function ( ) {
         if ($scope.user.pass.localeCompare($scope.user.pass2) == 0){
-          localStorageService.set("user-lep", $scope.user);
-          $location.path("/");
+          tripService.createUser($scope.user.dni,$scope.user.pass,$scope.user.name,$scope.user.lastname,$scope.user.email).then(function(origins){
+            if (origins != null && origins[0] != null){
+              localStorageService.set("user-lep", $scope.user);
+              localStorageService.set("rld", "yes");
+              location.reload();
+            } else {
+              alert("Usted ya tiene una cuenta creada");
+            }
+          }); 
         } else {
           alert('Las contraseñas no coinciden');
         }
@@ -44,21 +56,42 @@ angular.module('app')
 
     $scope.editPass = function ( ) {
         if ($scope.user.pass.localeCompare($scope.user.pass2) == 0){
-          localStorageService.set("user-lep", $scope.user);
-          $location.path("/");
+          var oldPass = localStorageService.get("user-lep");
+          tripService.editPass($scope.user.dni,oldPass.pass,$scope.user.pass,$scope.user.email).then(function(origins){
+            if (origins == 1) {
+                localStorageService.set("user-lep", $scope.user);
+                $location.path("/");
+              } else {
+                alert("No se ha podido editadar la contraseña");
+              }
+          }); 
         } else {
           alert('Las contraseñas no coinciden');
         }
     };
 
     $scope.editProfile = function ( ) {
-          localStorageService.set("user-lep", $scope.user);
-          $location.path("/");
+          tripService.editProfile($scope.user.dni,$scope.user.name,$scope.user.lastname,$scope.user.email).then(function(origins){
+            if (origins != null && origins[0] != null){
+              localStorageService.set("user-lep", $scope.user);
+              $location.path("/");
+            } else {
+              alert("No se ha podido editar el perfil");
+            }
+          }); 
     };
 
     
     $scope.recoverPass = function ( ) {
+      tripService.recoverPass($scope.user.dni,$scope.user.email).then(function(origins){
+        console.log(origins);
+        if (origins == 1) {
+          alert("Se ha enviado a su email la contraseña");
           $location.path("/");
+        } else {
+          alert("La cuenta no existe o no esta activada");
+        }
+      }); 
     };
 
 });
