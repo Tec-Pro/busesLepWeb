cf9f3b3d3beb45704f83b83c4b6b29d1f57896c7
agusbauer <agustinbauer1@gmail.com>
2016-05-09 21:02:55 -0300

    summary,seats and details views conected

--- js/controllers/seatPickerController.js
+++ js/controllers/seatPickerController.js
@@ -2,9 +2,24 @@ angular.module('app').controller('SeatsController', function ($scope, $location)
 
 	
 		$scope.seats = [];
-		for (var i = 0; i < 80; i++) {
-			$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: i});
+		$scope.seats.push({ img: 'img/driver_seat.png',status: 4, id: 0});
+		$scope.seats.push({ img: 'img/none_seat.png',status: 0, id: 1});
+		$scope.seats.push({ img: 'img/none_seat.png',status: 0, id: 2});
+		$scope.seats.push({ img: 'img/none_seat.png',status: 0, id: 3});
+		$scope.seats.push({ img: 'img/none_seat.png',status: 0, id: 4});
+		for (var i = 5; i < 65; i++) {
+			if(i % 5 == 2 || i % 5 == 3){
+				$scope.seats.push({ img: 'img/none_seat.png',status: 0, id: i});
+			}
+			else{
+				$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: i});
+			}
 		};
+		$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: 65});
+		$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: 66});
+		$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: 67});
+		$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: 68});
+		$scope.seats.push({ img: 'img/free_seat.png',status: 1, id: 69});
 
 	    $scope.toggleImage = function(seat) {
 	    	switch($scope.seats[seat.id].status) {
@@ -25,6 +40,10 @@ angular.module('app').controller('SeatsController', function ($scope, $location)
 	    $scope.goDetails= function() {
 	    	$location.path('/details');	 
 		};
+
+		$scope.goBack = function () {
+			window.history.back();
+		}
 
 		$scope.range = function(min, max, step){
 		    step = step || 1;
