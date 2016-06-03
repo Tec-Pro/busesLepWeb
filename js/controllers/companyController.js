47ce425c1b2192121d8480fd54ba0765991d61e3
jheredia <joaquin.heredia93@gmail.com>
2016-06-03 00:37:21 -0300

    Cleaning company code

--- js/controllers/companyController.js
+++ js/controllers/companyController.js
@@ -1,39 +1,9 @@
 angular.module('app')
 .controller('CompCtrl', function($scope, $location){
 
-	$scope.hide-history = false;
-	$scope.hide-about-us = true;
-	$scope.hide-contact-us = true;
-	$scope.hide-units = true;
+	$scope.active = 1;
 
 	$scope.set_active = function(tab){
-		switch(tab){
-			case 1: 
-				$scope.hide-history = false;
-				$scope.hide-about-us = true;
-				$scope.hide-contact-us = true;
-				$scope.hide-units = true;
-				break;
-			case 2:
-				$scope.hide-history = true;
-				$scope.hide-about-us = false;
-				$scope.hide-contact-us = true;
-				$scope.hide-units = true;
-				break;
-			case 3:
-				$scope.hide-history = true;
-				$scope.hide-about-us = true;
-				$scope.hide-contact-us = false;
-				$scope.hide-units = true;
-				break;
-			case 4:
-				$scope.hide-history = true;
-				$scope.hide-about-us = true;
-				$scope.hide-contact-us = true;
-				$scope.hide-units = false;
-				break;
-			default:
-				break;
-		}
+		$scope.active = tab;
 	}
 });
