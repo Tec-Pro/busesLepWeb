65e16b50c2223a1d00873a39059da9aeccde24a9
jheredia <joaquin.heredia93@gmail.com>
2016-06-06 16:45:44 -0300

    Added unit tabs

--- js/controllers/companyController.js
+++ js/controllers/companyController.js
@@ -2,8 +2,13 @@ angular.module('app')
 .controller('CompCtrl', function($scope, $location){
 
 	$scope.active = 1;
+	$scope.active_unit_tab = 1;
 
 	$scope.set_active = function(tab){
 		$scope.active = tab;
+	}
+
+	$scope.set_active_unit_tab = function(tab){
+		$scope.active_unit_tab = tab;
 	}
 });
