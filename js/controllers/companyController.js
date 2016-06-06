ca65fed4b143341e9f07a2af07693d6391bc7f29
jheredia <joaquin.heredia93@gmail.com>
2016-06-06 18:47:25 -0300

    Gallery scratch

--- js/controllers/companyController.js
+++ js/controllers/companyController.js
@@ -11,4 +11,27 @@ angular.module('app')
 	$scope.set_active_unit_tab = function(tab){
 		$scope.active_unit_tab = tab;
 	}
+
+	$scope.gallery_thumbnails = [
+		{
+			source: "./img/company/thumbnail1.png",
+			full_size: ""
+		},
+		{
+			source: "./img/company/thumbnail2.png",
+			full_size: ""
+		},
+		{
+			source: "./img/company/thumbnail3.png",
+			full_size: ""
+		},
+		{
+			source: "./img/company/thumbnail4.png",
+			full_size: ""
+		},
+		{
+			source: "./img/company/thumbnail5.png",
+			full_size: ""
+		}
+	]
 });
