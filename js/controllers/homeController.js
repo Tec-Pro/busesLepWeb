2aec58c7f4db9fdc23d05a74ee1cbf8c91da9f81
jheredia <joaquinheredia93@gmail.com>
2016-07-22 16:09:12 -0300

    Modifications to Special and Home

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -73,6 +73,7 @@ angular.module('app')
 
     //Function that check the availables destinations whenever the trip origin changes.
     $scope.checkDestinations = function(origin){
+        console.log(origin);
         $scope.params.origin = origin;
         $scope.origin_search = origin.Localidad;
       	//Enable the destination picker if the origin has been set.
@@ -157,76 +158,80 @@ angular.module('app')
       var b = $scope.params.returnDate;
       //a must be != null, b can be null.
       //if is a round trip, and b (The return date) is after the a (The departure date) alert the user.
-      if ((b != '') && (a.isAfter(b, 'day'))) {
-          	window.alert("La fecha de llegada no puede ser anterior a la de salida");
+      if (a === ''){
+        window.alert("Por favor elija una fecha de salida");
       } else {
-		  //Else redirect to schedules and set the data in the service.
-				if (b != ''){
-					tripService.setRoundTrip(1);
-					tripService.setTripReturn(b);
-				} else {
-					tripService.setRoundTrip(0);
-					tripService.setTripReturn('');
-				}
-				tripService.setTripOriginId($scope.params.origin.ID_Localidad);
-				tripService.setTripOriginName($scope.params.origin.Localidad);
-				tripService.setTripDestinationId($scope.params.destination.id_localidad_destino);
-				tripService.setTripDestinationName($scope.params.destination.hasta);
-        //console.log($scope.params.departureDate);
-				tripService.setTripDeparture($scope.params.departureDate);
-				// tripService.searchTrips($scope.params.origin.ID_Localidad, $scope.params.destination.id_localidad_destino, $scope.params.departureDate.format("YYYYMMDD")).then(function(schedules)
-        var listarHorarios_parameters = [
-          {
-            name: "userWS",
-            type: "string",
-            value: "UsuarioLep"
-          },
-          {
-            name: "passWS",
-            type: "string",
-            value: "Lep1234"
-          },
-          {
-            name: "IdLocalidadOrigen",
-            type: "int",
-            value: $scope.params.origin.ID_Localidad
-          },
-          {
-            name: "IdLocalidadDestino",
-            type: "int",
-            value: $scope.params.destination.id_localidad_destino
-          },
-          {
-            name: "Fecha",
-            type: "string",
-            value: $scope.params.departureDate.format("YYYYMMDD")
-          },
-          {
-            name: "DNI",
-            type: "int",
-            value: "1"
-          },
-          {
-            name: "id_plataforma",
-            type: "int",
-            value: "3"
-          }          
-        ];
-        tripService.saveDepartureTrip();
-        wsService.callService(wsdl_url, urn, "ListarHorarios", listarHorarios_parameters).then(function(schedules){
-						if (schedules.length > 0){
-              var ida = tripService.getDepartureTrip();
-              $scope.searches.unshift({ goingDate: ida.departure_date, backDate: ida.return_date, goingCity: ida.origin_name, backCity: ida.destination_name, status: false});
-              localStorageService.set("last-searches",JSON.stringify($scope.searches));
-              //guardar aca departure-trip
-              //tripService.saveDepartureTrip();
-							tripService.setSchedules(schedules);
-							$location.path('/schedules');
-						} else {
-							window.alert("No existen viajes para esa fecha");
-						}
-				});
-			
+        if ((b != '') && (a.isAfter(b, 'day'))) {
+            	window.alert("La fecha de llegada no puede ser anterior a la de salida");
+        } else {
+  		  //Else redirect to schedules and set the data in the service.
+  				if (b != ''){
+  					tripService.setRoundTrip(1);
+  					tripService.setTripReturn(b);
+  				} else {
+  					tripService.setRoundTrip(0);
+  					tripService.setTripReturn('');
+  				}
+  				tripService.setTripOriginId($scope.params.origin.ID_Localidad);
+  				tripService.setTripOriginName($scope.params.origin.Localidad);
+  				tripService.setTripDestinationId($scope.params.destination.id_localidad_destino);
+  				tripService.setTripDestinationName($scope.params.destination.hasta);
+          //console.log($scope.params.departureDate);
+  				tripService.setTripDeparture($scope.params.departureDate);
+  				// tripService.searchTrips($scope.params.origin.ID_Localidad, $scope.params.destination.id_localidad_destino, $scope.params.departureDate.format("YYYYMMDD")).then(function(schedules)
+          var listarHorarios_parameters = [
+            {
+              name: "userWS",
+              type: "string",
+              value: "UsuarioLep"
+            },
+            {
+              name: "passWS",
+              type: "string",
+              value: "Lep1234"
+            },
+            {
+              name: "IdLocalidadOrigen",
+              type: "int",
+              value: $scope.params.origin.ID_Localidad
+            },
+            {
+              name: "IdLocalidadDestino",
+              type: "int",
+              value: $scope.params.destination.id_localidad_destino
+            },
+            {
+              name: "Fecha",
+              type: "string",
+              value: $scope.params.departureDate.format("YYYYMMDD")
+            },
+            {
+              name: "DNI",
+              type: "int",
+              value: "1"
+            },
+            {
+              name: "id_plataforma",
+              type: "int",
+              value: "3"
+            }          
+          ];
+          tripService.saveDepartureTrip();
+          wsService.callService(wsdl_url, urn, "ListarHorarios", listarHorarios_parameters).then(function(schedules){
+  						if (schedules.length > 0){
+                var ida = tripService.getDepartureTrip();
+                $scope.searches.unshift({ goingDate: ida.departure_date, backDate: ida.return_date, goingCity: ida.origin_name, backCity: ida.destination_name, status: false});
+                localStorageService.set("last-searches",JSON.stringify($scope.searches));
+                //guardar aca departure-trip
+                //tripService.saveDepartureTrip();
+  							tripService.setSchedules(schedules);
+  							$location.path('/schedules');
+  						} else {
+  							window.alert("No existen viajes para esa fecha");
+  						}
+  				});
+  			
+        }
       }
     };
 
