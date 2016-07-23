41250b918832e20a028f87c7ce1b055e13eb70d3
jheredia <joaquinheredia93@gmail.com>
2016-07-23 17:55:05 -0300

    Addedd loading modal to schedules view

--- js/controllers/schedulesListController.js
+++ js/controllers/schedulesListController.js
@@ -3,6 +3,16 @@ angular.module('app').controller('ScheduleController', function ($scope, $locati
 	var wsdl_url_wsConGps = "https://webservices.buseslep.com.ar:443/WebServices/WebServiceLepcGPS.dll/soap/ILepWebService";
     var urn = 'LepWebServiceIntf-ILepWebService';
 
+    var loading_modal = document.getElementById('sched-modal');
+
+	var display_loading_modal = function() {
+		loading_modal.style.display = "block";
+	}
+
+	var hide_loading_modal = function() {
+		loading_modal.style.display = "none";
+	}
+
     
 
      getReturnSchedules = function(){
@@ -42,14 +52,22 @@ angular.module('app').controller('ScheduleController', function ($scope, $locati
             type: "int",
             value: "3"
           }          
-        ];
+        ];display_loading_modal();
         wsService.callService(wsdl_url, urn, "ListarHorarios", listarHorarios_parameters2).then(function(schedules){ //"ListarHorarioscGPS"
+			hide_loading_modal();
 			if (schedules.length > 0){
 				$scope.schedules = schedules;
 			} else {
 				window.alert("No existen viajes para esa fecha");
 			}
-		});
+		}, function(reason){
+		    if (reason == "timeout"){
+		        alert("Tiempo de respuesta agotado, verifique su conexión o intente más tarde.");
+		    } else {
+		    	alert("Error: "+reason+". Por favor, intente más tarde.");
+		    }
+		    hide_loading_modal();
+    	});
 	};
     
 	$scope.departure_trip = tripService.getDepartureTrip();
@@ -86,49 +104,58 @@ angular.module('app').controller('ScheduleController', function ($scope, $locati
 		//console.log($scope.params.departureDate);
     	if (!$scope.params.departureDate.isSame($scope.departure_date)){
 	        var listarHorarios_parameters = [
-	        {
-	            name: "userWS",
-	            type: "string",
-	            value: "UsuarioLep"
-	        },
-	        {
-	            name: "passWS",
-	            type: "string",
-	            value: "Lep1234"
-	        },
-	        {
-	            name: "IdLocalidadOrigen",
-	            type: "int",
-				value: $scope.origin_id
-	        },
-	        {
-	            name: "IdLocalidadDestino",
-	            type: "int",
-				value: $scope.destination_id
-	        },
-	        {
-	        	name: "Fecha",
-	          	type: "string",
-	          	value: $scope.params.departureDate.format("YYYYMMDD")
-	        },
-	        {
-	            name: "DNI",
-	            type: "int",
-	            value: "1"
-	        },
-	        {
-	            name: "id_plataforma",
-	            type: "int",
-	            value: "3"
-			}          
-        ];
-        wsService.callService(wsdl_url_wsConGps, urn, "ListarHorarioscGPS", listarHorarios_parameters).then(function(schedules){
-			if (schedules.length > 0){
-				$scope.schedules = schedules;
-			} else {
-				window.alert("No existen viajes para esa fecha");
-			}
-		});
+		        {
+		            name: "userWS",
+		            type: "string",
+		            value: "UsuarioLep"
+		        },
+		        {
+		            name: "passWS",
+		            type: "string",
+		            value: "Lep1234"
+		        },
+		        {
+		            name: "IdLocalidadOrigen",
+		            type: "int",
+					value: $scope.origin_id
+		        },
+		        {
+		            name: "IdLocalidadDestino",
+		            type: "int",
+					value: $scope.destination_id
+		        },
+		        {
+		        	name: "Fecha",
+		          	type: "string",
+		          	value: $scope.params.departureDate.format("YYYYMMDD")
+		        },
+		        {
+		            name: "DNI",
+		            type: "int",
+		            value: "1"
+		        },
+		        {
+		            name: "id_plataforma",
+		            type: "int",
+		            value: "3"
+				}          
+        	];
+        	display_loading_modal();
+	        wsService.callService(wsdl_url_wsConGps, urn, "ListarHorarioscGPS", listarHorarios_parameters).then(function(schedules){
+				hide_loading_modal();
+				if (schedules.length > 0){
+					$scope.schedules = schedules;
+				} else {
+					window.alert("No existen viajes para esa fecha");
+				}
+			}, function(reason){
+			    if (reason == "timeout"){
+			        alert("Tiempo de respuesta agotado, verifique su conexión o intente más tarde.");
+			    } else {
+			    	alert("Error: "+reason+". Por favor, intente más tarde.");
+			    }
+			    hide_loading_modal();
+    		});
 	
     	}
     });
@@ -155,12 +182,20 @@ angular.module('app').controller('ScheduleController', function ($scope, $locati
 			value: $scope.destination_id
 		}
 	]
-
+	display_loading_modal();
 	wsService.callService(wsdl_url, urn, 'ObtenerTarifaTramo', parametersPrice).then(function(tarifas){
+		hide_loading_modal();
  		result = tarifas.split("-");
  		$scope.goPrice = Number(result[0].trim().substring(7));
  		$scope.roundTripPrice = Number(result[1].trim().substring(13));
- 	});
+ 	}, function(reason){
+	    if (reason == "timeout"){
+	        alert("Tiempo de respuesta agotado, verifique su conexión o intente más tarde.");
+	    } else {
+	    	alert("Error: "+reason+". Por favor, intente más tarde.");
+	    }
+	    hide_loading_modal();
+    });
  	
 	//riocuarto es id=10 cordoba plaza es id=1
 	//Date picker options
@@ -244,8 +279,6 @@ angular.module('app').controller('ScheduleController', function ($scope, $locati
 
 		marker.setMap(map);
     };
-
-	
 
 	$scope.hide_modal = function(){
       map_modal.style.display = "none";
