ed96bc8c40392be621dcba2de412059e595ef149
jheredia <joaquinheredia93@gmail.com>
2016-07-25 16:00:28 -0300

    Added search clear on cities fields

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -69,6 +69,9 @@ angular.module('app')
   $scope.origins = [];
 
   $scope.load_origins = function(){
+    $scope.origin_search = '';
+    $scope.destination_search = '';
+    $scope.destinations = '';
     //Call the web service and update the origins from the scope.
     if ($scope.origins.length == 0){
       display_modal();
