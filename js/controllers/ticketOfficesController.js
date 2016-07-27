18396dd52e5af296255f9f2aecce80098825050c
jheredia <joaquinheredia93@gmail.com>
2016-07-27 17:59:46 -0300

    Modification to ticket offices controller

--- js/controllers/ticketOfficesController.js
+++ js/controllers/ticketOfficesController.js
@@ -1,14 +1,27 @@
-angular.module('app').controller('TicketOfficesController', ['$scope', '$location', function ($scope, $location){
-  	$scope.ticketOffices = [
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Boleterias/EjemploFotoBoleteria.png'},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: "img/Empresa/Confort_imagen2.png"},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Boleterias/EjemploFotoBoleteria.png'},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Empresa/Confort_imagen1.png'},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Empresa/Confort_imagen3.png'},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Boleterias/EjemploFotoBoleteria.png'},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Empresa/Confort_imagen2.png'},
-	    { location: 'Adelia Maria', address: 'Terminal de Omnibus', phone: '03585 421541 / 422300', img: 'img/Boleterias/EjemploFotoBoleteria.png'}
-  	];
+angular.module('app').controller('TicketOfficesController', ['$scope', '$location','wsService', function ($scope, $location, wsService){
+  	
+  	wsdl_url = "https://webservices.buseslep.com.ar:443/WebServices/WebServiceLepcGPS.dll/soap/ILepWebService";
+	urn = "LepWebServiceIntf-ILepWebService";
+	method = "Boleterias";
+
+	var ticketOfficesParams = [
+		{
+			name: "userWS",
+		    type: "string",
+		    value: "UsuarioLep"
+		},
+		{
+			name: "passWS",
+		    type: "string",
+		    value: "Lep1234"
+		}
+	];
+
+	$scope.ticketOffices = [];
+
+	wsService.callService(wsdl_url, urn, method, ticketOfficesParams).then(function(offices){
+		$scope.ticketOffices = offices;
+	});
 
   	$scope.selectedImg = "img/Boleterias/EjemploFotoBoleteria.png";
 
@@ -20,7 +33,7 @@ angular.module('app').controller('TicketOfficesController', ['$scope', '$locatio
 	$scope.selectedRow = null;  // initialize our variable to null
   	$scope.setClickedRow = function(index){  //function that sets the value of selectedRow to current index
     	$scope.selectedRow = index;
-    	$scope.selectedImg = $scope.ticketOffices[index].img;
+    	$scope.selectedImg = $scope.ticketOffices[index].linkfoto;
   	}
 
 }]);
