5a709caf3a51e7a9da6407ecaef268bebe9b062a
agusbauer <agustinbauer1@gmail.com>
2016-07-28 00:17:53 -0300

    tours in progress

--- js/controllers/toursController.js
+++ js/controllers/toursController.js
@@ -1,9 +1,41 @@
-angular.module('app').controller('ToursController', ['$scope', '$location', function ($scope, $location){
+angular.module('app').controller('ToursController', ['$scope', '$location','wsService', function ($scope, $location, wsService){
 
-  	$scope.tours = [
+	var wsdl_url = "https://webservices.buseslep.com.ar:443/WebServices/WebServiceLepcGPS.dll/soap/ILepWebService";
+
+	var toursParams = [
+		{
+			name: "userWS",
+		    type: "string",
+		    value: "UsuarioLep"
+		},
+		{
+			name: "passWS",
+		    type: "string",
+		    value: "Lep1234"
+		}
+	];
+
+	$scope.tours = [];
+	wsService.callService(wsdl_url, "", "Destinos", toursParams).then(function(response){
+
+		for (var i = 0; i < response.length; i++) {
+			splittedOrigAndDest = response[i].DestinoWeb.split("-");
+			splittedCities2 = response[i].Recorridos.split(" . ");
+			citiesArray = [];
+			for (var j = 0 ; j < splittedCities2.length; j++) {
+				citiesArray.push({name: splittedCities2[j]});
+			};
+			
+			$scope.tours.push({origin: splittedOrigAndDest[0],destination: splittedOrigAndDest[splittedOrigAndDest.length-1], 'cities': citiesArray});
+		};
+	});
+
+	
+
+  	/*$scope.tours = [
 	    { origin: 'Cordoba', destination: 'Sta Rosa de Calamuchita', 'cities':[{name: "Va. Gral Belgrano"}]},
 	    { origin: 'Sta Rosa de Calamuchita', destination: 'Amancay', 'cities':[{name: "El torreon"}, {name: "San Ignacio"}, {name: "Amboy"}]},
 	    { origin: 'Va. Gral Belgrano', destination: 'Va. Ciudad Parque', 'cities':[{name: "Los Reartes"}]},
-  	];
+  	];*/
 
  }]);
