f125095ec3380e4504c1aef0cabb9302150c0d8e
lputruele <lputruele@gmail.com>
2016-07-30 18:28:42 -0300

    noticias?

--- js/controllers/footerController.js
+++ js/controllers/footerController.js
@@ -1,15 +1,37 @@
 angular.module('app')
 .controller('FooterCtrl', ['$scope', '$location', '$http', '$window', 'localStorageService', 'companyService', 'wsService', 'feedService', function ($scope, $location, $http, $window, localStorageService, companyService, wsService,feedService) {
+	//CLIMA
 	$http.get("http://api.openweathermap.org/data/2.5/weather?id=3860259&APPID=6b10444e5758a2d047de8e60b9fd368d")
 	.then(function (response){
 		$scope.weather = {temp: response.data.main.temp - 273.15,
 						  tempMin: response.data.main.temp_min - 273.15,
 						  tempMax: response.data.main.temp_max - 273.15};
 	});
-
+	//FEED
 	feedService.parseFeed('http://www.lavoz.com.ar/rss.xml').then(function(res){
 		$scope.feeds = res.data.responseData.feed.entries;
 	});
+	//NOTICIAS Y NOVEDADES
+	var wsdl_url ="https://webservices.buseslep.com.ar:443/WebServices/WSLepPaginaWeb.dll/soap/IWSLepPaginaWeb";
+	var urn = 'LepWebServiceIntf-ILepWebService';
+	var parameters = [
+    {
+      name: "userWS",
+      type: "string",
+      value: "UsuarioLep"
+    },
+    {
+      name: "passWS",
+      type: "string",
+      value: "Lep1234"
+    }];
+
+	wsService.callService(wsdl_url, urn, "Noticias", parameters).then(function(response){
+    	$scope.noticias = response;
+    });
+	wsService.callService(wsdl_url, urn, "Novedades", parameters).then(function(response){
+        $scope.novedades = response;
+    });
 
 	$scope.goCompanyContact = function(){
       companyService.setActiveTab(2);
