5e7d6e2a0471c5ecc053a22bb5d78788965bd704
jheredia <joaquinheredia93@gmail.com>
2016-10-05 13:12:16 -0300

    Possible fix for mercadopago

--- js/controllers/mercadopagoController.js
+++ js/controllers/mercadopagoController.js
@@ -1,46 +1,67 @@
 angular.module('app')
 .controller('MercadopagoController', ['$scope', '$http', '$location', '$anchorScroll', 'wsService', 'localStorageService', 'tripService', function($scope,$http, $location, $anchorScroll, wsService, localStorageService, tripService){
 //https://webservices.buseslep.com.ar:443/WebServices/WSCobroMercadoPagoTest.dll/soap/IWSCobroMercadoPago
-//http://webservices.buseslep.com.ar:8080/WebServices/WSCobroMercadoPagoTest.dll/soap/IWSCobroMercadoPago
 	//https://webservices.buseslep.com.ar/WebServices/WSCobroMercadoPago.dll/wsdl/IWSCobroMercadoPago para poner en el soap client
 	//https://webservices.buseslep.com.ar:443/WebServices/WSCobroMercadoPago.dll/soap/IWSCobroMercadoPago //para poner en la wsdl_urn
-	var wsdl_url ="https://webservices.buseslep.com.ar:443/WebServices/WSCobroMercadoPago.dll/soap/IWSCobroMercadoPago"; //"https://webservices.buseslep.com.ar:443/WebServices/WSCobroMercadoPagocTestyEnc.dll/soap/IWSCobroMercadoPago";
+	var wsdl_url ="https://webservices.buseslep.com.ar:443/WebServices/WSCobroMercadoPagoTest.dll/soap/IWSCobroMercadoPago"; //"https://webservices.buseslep.com.ar:443/WebServices/WSCobroMercadoPagocTestyEnc.dll/soap/IWSCobroMercadoPago";
 	var urn = "";
 	wsMethod = "RealizarCobroMercadoPago";
 	$anchorScroll();
-	Mercadopago.setPublishableKey("APP_USR-3f8dc194-8894-4d07-bb6c-b4a786a19c6c"); //TEST-2e5d7d95-7cb8-48d3-8bd6-cfde1bc34254
-	$scope.paymentMethods = [];													//APP_USR-3f8dc194-8894-4d07-bb6c-b4a786a19c6c
-	$http.get("https://api.mercadolibre.com/sites/MLA/payment_methods").success(function(response){ //llama a la api nuestra y ahi se obtiene los medios de pago
-		//obj = JSON.parse(response)
-		payments = response;
-		if (payments.length > 0) {
-			for (var i = 0; i < payments.length; i++) {
-				if(payments[i].payment_type_id == 'credit_card'){
-					$scope.paymentMethods.push({id:payments[i].id,name:payments[i].name});
-				}
-			};
-    	}
-    });
-    var user_email = localStorageService.get("user-lep").email;
-    document.querySelector('#email').value = user_email;
-	var sell_code = tripService.getSellCode();
-	document.querySelector('#amount').value = tripService.getTripPrice();//10//tripService.getTripPrice();
+	$scope.paymentMethod = [];
+
+	//Sets the test key.
+	Mercadopago.setPublishableKey("TEST-2e5d7d95-7cb8-48d3-8bd6-cfde1bc34254"); 
+	//TEST-2e5d7d95-7cb8-48d3-8bd6-cfde1bc34254
+	
+	Mercadopago.getIdentificationTypes();
+
+	$scope.issuer = '';
+	$scope.installment = '';
+	$scope.object = {
+		"bin": ''
+	}
+
+	$scope.paymentMethod = [];
+	var show_issuers = false;
+	var enable_installments = false;
+
+/*	var binHandler = function(){
+		if 
+	}
+*/
+	var error_modal = document.getElementById('error-modal');
+	var load_modal = document.getElementById('load-modal');
+
+   var display_load_modal = function(){
+    load_modal.style.display = "block";
+  }
+
+  var hide_load_modal = function(){
+    load_modal.style.display = "none";
+  }
+
+  $scope.hide_modal = function(){
+    error_modal.style.display = "none";
+    location.reload();
+  }
+  var user_email = localStorageService.get("user-lep").email;
+  if (user_email == undefined){
+  	$location.path('/');
+  }
+
+	$scope.price = tripService.getTripPrice();//10//tripService.getTripPrice();
 	if(tripService.getTripPrice() == undefined){
 		$location.path('/');
 	}
-	$scope.selectedPayment = null;
-	$scope.totalAmount = document.querySelector('#amount').value;
-   	$scope.showSecurityCodeInput = true;
-   	$scope.onlyNumbers = /^\d+$/;
-	Mercadopago.getIdentificationTypes(); // obtengo los tipos de documentos y se ponen solos en el select del html!
-
-	/*Mercadopago.getInstallments({
-			"payment_method_id":"master",
-			"amount": 100
-		}, function (status, response){
-			//console.log(response);
-	});*/
-
+	var sell_code = tripService.getSellCode();
+	if(sell_code == undefined){
+		$location.path('/');
+	}
+	var remove_options = function(obj){
+		if (obj == null) return;
+		if (obj.options == null) return;
+		obj.options.length = 1;
+	}
 
 
 	function addEvent(el, eventName, handler){
@@ -53,236 +74,127 @@ angular.module('app')
 	    }
 	};
 
-	function getBin() {
-		var cardSelector = document.querySelector("#cardId");
-	    if (cardSelector && cardSelector[cardSelector.options.selectedIndex].value != "-1") {
-	        return cardSelector[cardSelector.options.selectedIndex].getAttribute('first_six_digits');
-	    }
-	    var ccNumber = document.querySelector('input[data-checkout="cardNumber"]');
-	    return ccNumber.value.replace(/[ .-]/g, '').slice(0, 6);
-	};
-
-	function clearOptions() {
-	    var bin = getBin();
-	    if (bin.length == 0) {
-	        document.querySelector("#issuer").style.display = 'none';
-	        document.querySelector("#issuer").innerHTML = "";
-
-	        var selectorInstallments = document.querySelector("#installments"),
-	            fragment = document.createDocumentFragment(),
-	            option = new Option("Choose...", '-1');
-
-	        selectorInstallments.options.length = 0;
-	        fragment.appendChild(option);
-	        selectorInstallments.appendChild(fragment);
-	        selectorInstallments.setAttribute('disabled', 'disabled');
-	    }
-	}
-
-
-	$scope.paymentMethodSelected = function(){
-    	//console.log($scope.selectedPayment);
-    	amount = document.querySelector('#amount').value;
-    	Mercadopago.getPaymentMethod({"payment_method_id": $scope.selectedPayment}, setPaymentMethodInfo);
- 	 }
-	/*function guessingPaymentMethod(event) {
-
-	    var bin = getBin();
-	    amount = document.querySelector('#amount').value;
-	    if (event.type == "keyup") {
-	        if (bin.length >= 6) {
-	            Mercadopago.getPaymentMethod({
-	                "bin": bin
-	            }, setPaymentMethodInfo);
-	        }
-	    } else {
-	        setTimeout(function() {
-	            if (bin.length >= 6) {
-	                Mercadopago.getPaymentMethod({
-	                    "bin": bin
-	                }, setPaymentMethodInfo);
-	            }
-	        }, 100);
-	    }
-	};*/
-
-	function setPaymentMethodInfo(status, response) {
-
-	    if (status == 200) {
-	        // do somethings ex: show logo of the payment method
-	        var form = document.querySelector('#pay');
-
-	        if (document.querySelector("input[name=paymentMethodId]") == null) {
-	            var paymentMethod = document.createElement('input');
-	            paymentMethod.setAttribute('name', "paymentMethodId");
-	            paymentMethod.setAttribute('type', "hidden");
-	            paymentMethod.setAttribute('value', response[0].id);
-	            //console.log(response[0].id);
-	            form.appendChild(paymentMethod);
-	        } else {
-	            document.querySelector("input[name=paymentMethodId]").value = response[0].id;
-	        }
-
-	         // check if the security code (ex: Tarshop) is required
-	        var cardConfiguration = response[0].settings,
-	            bin = getBin(),
-	            amount = document.querySelector('#amount').value;
-
-	        for (var index = 0; index < cardConfiguration.length; index++) {
-	            if (bin.match(cardConfiguration[index].bin.pattern) != null && cardConfiguration[index].security_code.length == 0) {
-	                /*
-	                * In this case you do not need the Security code. You can hide the input.
-	                */
-	                $scope.showSecurityCodeInput = false;
-	            } else {
-	            	$scope.showSecurityCodeInput = true;
-	                /*
-	                * In this case you NEED the Security code. You MUST show the input.
-	                */
-	            }
-	        }
-
-	        Mercadopago.getInstallments({
-	            "payment_method_id": response[0].id,
-	            "amount": amount
-	        }, setInstallmentInfo);
-
-	        // check if the issuer is necessary to pay
-	        var issuerMandatory = false,
-	            additionalInfo = response[0].additional_info_needed;
-
-	        for (var i = 0; i < additionalInfo.length; i++) {
-	            if (additionalInfo[i] == "issuer_id") {
-	                issuerMandatory = true;
-	            }
-	        };
-	        if (issuerMandatory) {
-
-	            Mercadopago.getIssuers(response[0].id, showCardIssuers);
-
-	            addEvent(document.querySelector('#issuer'), 'change', setInstallmentsByIssuerId);
-
-	        } else {
-
-	            document.querySelector("#issuer").style.display = 'none';
-	            document.querySelector("#issuer").options.length = 0;
-	        }
+	$scope.$watch('object.bin', function(){
+		if ($scope.object.bin.length == 6){
+			Mercadopago.getPaymentMethod($scope.object, function(status, pm_response){
+				console.log(pm_response);
+				var form = document.querySelector('#pay');
+        if (document.querySelector("input[name=paymentMethodId]") == null) {
+            var paymentMethod = document.createElement('input');
+            paymentMethod.setAttribute('name', "paymentMethodId");
+            paymentMethod.setAttribute('type', "hidden");
+            paymentMethod.setAttribute('value', pm_response[0].id);
+            //console.log(response[0].id);
+            form.appendChild(paymentMethod);
+        } else {
+            document.querySelector("input[name=paymentMethodId]").value = pm_response[0].id;
+        }
+				if (pm_response.length > 1){
+					$scope.paymentMethod = pm_response[1];
+				} else {
+					$scope.paymentMethod = pm_response[0];
+				}
+				if ($scope.paymentMethod.additional_info_needed.includes("issuer_id")){
+					Mercadopago.getIssuers($scope.paymentMethod.id, function(status, iss_response){
+						document.getElementById('inst_sel').disabled = true;
+						if (status == 200){
+							var iss_sel =document.getElementById('issuerSelector');
+							remove_options(iss_sel);
+							for (var i = 0; i < iss_response.length; i++){
+								var iss_opt = document.createElement('option');
+								iss_opt.value = iss_response[i].id;
+								iss_opt.innerHTML = iss_response[i].name;
+								iss_sel.appendChild(iss_opt);
+							}
+							console.log(iss_response);
+							document.getElementById('iss_container').style.visibility = "visible";
+							/*$scope.installments.length = 0;*/
+						}
+					});
+				} else {
+					document.getElementById('iss_container').style.visibility = "hidden";
+					/*$scope.issuers.length = 0; */
+					Mercadopago.getInstallments({"bin":$scope.object.bin,"amount":$scope.price}, function(status, inst_response){
+						console.log(inst_response);
+						if (status == 200){
+							var inst_sel = document.getElementById('inst_sel');
+							remove_options(inst_sel);
+							for (var k = 0; k < inst_response[0].payer_costs.length; k++){
+								var inst_opt = document.createElement('option');
+								inst_opt.value = inst_response[0].payer_costs[k].installments;
+								inst_opt.innerHTML = inst_response[0].payer_costs[k].recommended_message;
+								inst_sel.appendChild(inst_opt);
+							}
+							inst_sel.disabled = false;
+							console.log(inst_sel);
+							//$scope.installments = inst_response;
+							//console.log(inst_response);
+							//console.log($scope.installments);
+							//console.log($scope.installments.length);
+						} else {
+							inst_sel.disabled = true;
+						}
+					});
+				}
+			})
+		} else if($scope.object.bin.length < 6) {
+/*			$scope.paymentMethod.length = 0;
+			$scope.issuers.length = 0;
+			$scope.installments.length = 0;*/
+			$scope.paymentMethod = [];
 		}
-	};
 
-	function showCardIssuers(status, issuers) {
-	    var issuersSelector = document.querySelector("#issuer"),
-	        fragment = document.createDocumentFragment();
+	});
 
-	    issuersSelector.options.length = 0;
-	    var option = new Option("Choose...", '-1');
-	    fragment.appendChild(option);
-	    for (var i = 0; i < issuers.length; i++) {
-	        if (issuers[i].name != "default") {
-	            option = new Option(issuers[i].name, issuers[i].id);
-	        } else {
-	            option = new Option("Otro", issuers[i].id);
-	        }
-	        fragment.appendChild(option);
-	    }
-	    issuersSelector.appendChild(fragment);
-	    issuersSelector.removeAttribute('disabled');
-	    document.querySelector("#issuer").removeAttribute('style');
-
-	};
-
-	function setInstallmentsByIssuerId(status, response) {
-	    var issuerId = document.querySelector('#issuer').value,
-	        amount = document.querySelector('#amount').value;
-
-	    if (issuerId === '-1') {
-	    	  var selectorInstallments = document.querySelector("#installments"),
-	            fragment = document.createDocumentFragment(),
-	            option = new Option("Choose...", '-1');
-
-	        selectorInstallments.options.length = 0;
-	        fragment.appendChild(option);
-	        selectorInstallments.appendChild(fragment);
-	        selectorInstallments.setAttribute('disabled', 'disabled');
-	        return;
-	    }
-
-	    Mercadopago.getInstallments({
-	        "payment_method_id": $scope.selectedPayment,
-	        "amount": amount,
-	        "issuer_id": issuerId
-	    }, setInstallmentInfo);
-	};
-
-	function setInstallmentInfo(status, response) {
-	    var selectorInstallments = document.querySelector("#installments"),
-	        fragment = document.createDocumentFragment();
-
-	    selectorInstallments.options.length = 0;
-
-	    if (response.length > 0) {
-	        var option = new Option("Choose...", '-1'),
-	        payerCosts = response[0].payer_costs;
-
-	        fragment.appendChild(option);
-					//Replace with this payerCosts.length
-	        for (var i = 0; i < 1; i++) {
-	            option = new Option(payerCosts[i].recommended_message || payerCosts[i].installments, payerCosts[i].installments);
-	            fragment.appendChild(option);
-	        }
-	        selectorInstallments.appendChild(fragment);
-	        selectorInstallments.removeAttribute('disabled');
-	    }
-	};
-
-	function cardsHandler() {
-	    clearOptions();
-	    var cardSelector = document.querySelector("#cardId"),
-	        amount = document.querySelector('#amount').value;
-	    if (cardSelector && cardSelector[cardSelector.options.selectedIndex].value != "-1") {
-	        var _bin = cardSelector[cardSelector.options.selectedIndex].getAttribute("first_six_digits");
-	        Mercadopago.getPaymentMethod({
-	            "bin": _bin
-	        }, setPaymentMethodInfo);
-	    }
+	$scope.issuerSelected = function(){
+		if($scope.issuer != ''){
+			Mercadopago.getInstallments({"bin":$scope.object.bin.substring(0,6), "payment_method_id": $scope.paymentMethod.id, "amount":$scope.price,"issuer_id":$scope.issuer}, function(status, inst_response){
+				//console.log({"bin":$scope.object.bin.substring(0,6), "amount":$scope.price,"issuer_id":$scope.issuer});
+				//console.log(inst_response);
+				console.log(inst_response);
+						if (status == 200){
+							var inst_sel = document.getElementById('inst_sel');
+							remove_options(inst_sel);
+							for (var k = 0; k < inst_response[0].payer_costs.length; k++){
+								var inst_opt = document.createElement('option');
+								inst_opt.value = inst_response[0].payer_costs[k].installments;
+								inst_opt.innerHTML = inst_response[0].payer_costs[k].recommended_message;
+								inst_sel.appendChild(inst_opt);
+							}
+							console.log(inst_sel);
+							inst_sel.disabled = false;
+							console.log(inst_sel);
+							//$scope.installments = inst_response;
+							//console.log(inst_response);
+							//console.log($scope.installments);
+							//console.log($scope.installments.length);
+						} else {
+							console.log("Ehhh");
+							document.getElementById('inst_sel').disabled = true;
+						}
+				//$scope.$apply();
+			})
+		}
 	}
 
-	addEvent(document.querySelector('input[data-checkout="cardNumber"]'), 'keyup', $scope.paymentMethodSelected);
-	addEvent(document.querySelector('input[data-checkout="cardNumber"]'), 'keyup', clearOptions);
-	addEvent(document.querySelector('input[data-checkout="cardNumber"]'), 'change', $scope.paymentMethodSelected);
-
-	cardsHandler();
 
 	doSubmit = false;
 	addEvent(document.querySelector('#pay'),'submit',doPay);
 	function doPay(event){
+			console.log("Hola amigo");
 	    event.preventDefault();
 	    if(!doSubmit){
 	        var $form = document.querySelector('#pay');
+	        console.log($form);
+	        console.log(document.getElementById('cardNumber'));
+	        console.log(document.getElementById('cardNumber').value);
 	        Mercadopago.createToken($form, sdkResponseHandler); // The function "sdkResponseHandler" is defined below
 	        return false;
 	    }
 	};
 
-	var error_modal = document.getElementById('error-modal');
-
-	    // Get the modal
-    var load_modal = document.getElementById('load-modal');
-
-    var display_load_modal = function(){
-      load_modal.style.display = "block";
-    }
-
-    var hide_load_modal = function(){
-      load_modal.style.display = "none";
-    }
-
-    $scope.hide_modal = function(){
-      error_modal.style.display = "none";
-      location.reload();
-    }
-
-	function sdkResponseHandler(status, response) {
+		function sdkResponseHandler(status, response) {
+			console.log(response);
 	    if (status != 200 && status != 201) {
 	    	document.getElementById("error-modal-text").innerHTML = "Datos Incorrectos";
 		    error_modal.style.display = "block";
@@ -298,7 +210,7 @@ angular.module('app')
 	        idventa = sell_code;
 	        //var form = new FormData(document.getElementById("form"));
 			//var inputValue = form.get("inputTypeName");
-			cuotas = document.querySelector('#installments').value;
+			cuotas = $scope.installment;
 			if (tripService.getPurchaseOrigin() == "0") {
 	        	var datosCompra = {description:"boletos",
 	        				external_reference: "boleto:"+idventa,
@@ -407,18 +319,19 @@ angular.module('app')
 			});*/
 	    }
 	};
+	//APP_USR-3f8dc194-8894-4d07-bb6c-b4a786a19c6c
 
-	function validate(evt) {
-	  var theEvent = evt || window.event;
-	  var key = theEvent.keyCode || theEvent.which;
-	  key = String.fromCharCode( key );
-	  var regex = /[0-9]|\./;
-	  if( !regex.test(key) ) {
-	    theEvent.returnValue = false;
-	    if(theEvent.preventDefault) theEvent.preventDefault();
-	  }
-	}
-
-
+	//Get the payment methods with an AJAX call.
+	/*$http.get("https://api.mercadolibre.com/sites/MLA/payment_methods").success(function(response){ 
+		payments = response;
+		if (payments.length > 0) {
+			for (var i = 0; i < payments.length; i++) {
+				//Filter the payments.
+				if(payments[i].payment_type_id == 'credit_card'){
+					$scope.paymentMethods.push({id:payments[i].id,name:payments[i].name});
+				}
+			};
+  	}	
+  });*/
 
 }]);
